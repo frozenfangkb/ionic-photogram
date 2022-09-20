f2e7d8ac3b585fcc18eb8cded45004711e4c75c5
David Rodriguez Miranda <wolfs_pride@outlook.es>
2022-09-20 11:44:57 +0200

    Start login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,25 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: '',
-    loadChildren: () => import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
+    path: 'main',
+    loadChildren: () =>
+      import('./pages/tabs/tabs.module').then((m) => m.TabsPageModule),
   },
   {
     path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
-  }
+    loadChildren: () =>
+      import('./pages/login/login.module').then((m) => m.LoginPageModule),
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
 ];
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule {}
--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,59 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
+  avatars = [
+    {
+      img: 'av-1.png',
+      seleccionado: true,
+    },
+    {
+      img: 'av-2.png',
+      seleccionado: false,
+    },
+    {
+      img: 'av-3.png',
+      seleccionado: false,
+    },
+    {
+      img: 'av-4.png',
+      seleccionado: false,
+    },
+    {
+      img: 'av-5.png',
+      seleccionado: false,
+    },
+    {
+      img: 'av-6.png',
+      seleccionado: false,
+    },
+    {
+      img: 'av-7.png',
+      seleccionado: false,
+    },
+    {
+      img: 'av-8.png',
+      seleccionado: false,
+    },
+  ];
 
-  constructor() { }
+  avatarSlide = {
+    slidesPerView: 3.5,
+  };
 
-  ngOnInit() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  login(fLogin: NgForm): void {
+    console.log(fLogin.valid);
   }
 
+  register(fRegister: NgForm): void {
+    console.log(fRegister.valid);
+  }
+
+  selectAvatar(avatar): void {
+    this.avatars.forEach((av) => (av.seleccionado = false));
+    avatar.seleccionado = true;
+  }
 }
